8f7d661193eed29af6ac56595d09cfc03f0a65d8
RakaShopp <raka.shopp4@gmail.com>
2025-06-17 00:01:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-function hideLoader() {
-  setTimeout(() => {
-    document.getElementById("loader").style.display = "none";
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
     document.querySelector("main").style.display = "block";
-  }, 1000);
-}
+  }, 1000); // loading cepat: 1 detik
+});
