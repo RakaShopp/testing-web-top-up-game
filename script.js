4760eb307dc1b223a96083073d0611caf281ecd5
RakaShopp <raka.shopp4@gmail.com>
2025-06-17 00:12:53 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    document.querySelector(".preloader").style.display = "none";
-    document.querySelector("main").style.display = "block";
-  }, 1000); // loading cepat: 1 detik
-});
